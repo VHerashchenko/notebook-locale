354a78b2cfc4eba4f51eed595753a6f87ac26768
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-09-07 19:01:22 +0300

    add ArgsConstructors to NoteModel

--- src/main/java/com/fict/vh/model/NoteModel.java
+++ src/main/java/com/fict/vh/model/NoteModel.java
@@ -1,13 +1,26 @@
 package com.fict.vh.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NoteModel {
     private String firstName;
     private String login;
     private String email;
     private String phoneNumber;
+
+    public NoteModel(NoteModel noteModel){
+        this.firstName = noteModel.getFirstName();
+        this.login = noteModel.getLogin();
+        this.email = noteModel.getEmail();
+        this.phoneNumber = noteModel.getPhoneNumber();
+    }
 
     public String getModelNotes(){
         return "Noted: " + firstName + " " + login
